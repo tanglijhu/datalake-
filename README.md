64e4bd594e44b813f9eb3bfc023005992eee4adb
Li Tang <38803555+tanglijhu@users.noreply.github.com>
2021-03-10 09:48:34 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+## Introduction
+The project objective is to help a music streaming startup, Sparkify, to move their date warehouse to a data lake. Specifically, an ETL pipeline was built to extract its data from AWS S3 and process them using Spark, and then load the data into a new S3 as a set of dimensional tables. This will allow their analytics team to continue finding insights in what songs their users are listening to.
+
+## Dataset
+Datasets used in this project are provided in two public S3 buckets. Datasets used in this project were provided in two public S3 buckets, the objects in which are csv files. One bucket contains information about songs and artists, the other bucket contains information about users' listening activities. 
+
+## Database Schema
+A star schema was optimized for queries on song play analysis as follows:
+
+### Fact Table
+- songplays - records in activity data regarding with song playing i.e. records with page NextSong
+
+### Dimension Tables
+- users - user information
+- songs - songs in music database
+- artists - artists in music database
+- time - timestamps for recording songplays broken down into specific time units 
+
+### The database schema is shown as follows:
+
+![database schema diagram](/img/datalake-databse-scema.PNG)
+
+## Spark Process
+The ETL job processes the song files then the log files. The song files are listed and iterated over entering relevant information in the artists and the song folders in parquet. The log files are filtered by the NextSong action. The subsequent dataset is then processed to extract the date, time, year etc. fields and records are then appropriately entered into the time, users and songplays folders in parquet for analysis.
+
+## Project Structure
+- etl.py - The ETL to reads data from S3, processes that data using Spark, and then writes them to a new S3
+- dl.cfg - Configuration file that contains info about AWS credentials
---
+++ etl.py
@@ -0,0 +1,126 @@
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+
+
+config = configparser.ConfigParser()
+config.read('dl.cfg')
+
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+
+
+def create_spark_session():
+    spark = SparkSession \
+        .builder \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .getOrCreate()
+    return spark
+
+
+def process_song_data(spark, input_data, output_data):
+    # get filepath to song data file
+    song_data = os.path.join(input_data, "song-data/A/A/A/*.json")
+    
+    # read song data file
+    df = spark.read.json(song_data)
+
+    # extract columns to create songs table
+    songs_table = df['song_id', 'title', 'artist_id', 'year', 'duration']
+    songs_table = songs_table.dropDuplicates(['song_id'])
+    
+    # write songs table to parquet files partitioned by year and artist
+    songs_table.write.partitionBy('year', 'artist_id').parquet(os.path.join(output_data, 'songs.parquet'), 'overwrite')
+
+    # extract columns to create artists table
+    artists_table = df['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']
+    artists_table = artists_table.dropDuplicates(['artist_id']) 
+    
+    # write artists table to parquet files
+    artists_table.write.parquet(os.path.join(output_data, 'artists.parquet'), 'overwrite')
+
+
+def process_log_data(spark, input_data, output_data):
+    # get filepath to log data file
+    log_data = os.path.join(input_data, "log_data/*/*/*.json")
+
+    # read log data file
+    df = spark.read.json(log_data)
+    
+    # filter by actions for song plays
+    songplays_table = df['ts', 'userId', 'level','sessionId', 'location', 'userAgent'] 
+
+    # extract columns for users table    
+    artists_table = df['userId', 'firstName', 'lastName', 'gender', 'level']
+    artists_table = artists_table.dropDuplicates(['userId'])
+    
+    # write users table to parquet files
+    artists_table.write.parquet(os.path.join(output_data, 'users.parquet'), 'overwrite')
+    print("users.parquet completed")
+
+    # create timestamp column from original timestamp column
+    get_timestamp = udf(lambda x: str(int(int(x)/1000)))
+    df = df.withColumn('timestamp', get_timestamp(df.ts)) 
+    
+    # create datetime column from original timestamp column
+    get_datetime = udf(lambda x: str(datetime.fromtimestamp(int(x) / 1000.0)))
+    df = df.withColumn("datetime", get_datetime(df.ts))
+    
+    # extract columns to create time table
+    time_table = df.select(
+        col('datetime').alias('start_time'),
+        hour('datetime').alias('hour'),
+        dayofmonth('datetime').alias('day'),
+        weekofyear('datetime').alias('week'),
+        month('datetime').alias('month'),
+        year('datetime').alias('year') 
+   )
+    time_table = time_table.dropDuplicates(['start_time'])
+    
+    # write time table to parquet files partitioned by year and month
+    time_table.write.partitionBy('year', 'month').parquet(os.path.join(output_data, 'time.parquet'), 'overwrite')
+    print("time.parquet completed")
+
+    # read in song data to use for songplays table
+    song_data = os.path.join(input_data, "song-data/A/A/A/*.json")
+    song_df = spark.read.json(song_data) 
+
+    # extract columns from joined song and log datasets to create songplays table 
+    df = df.join(song_df, song_df.title == df.song)
+
+    songplays_table = df.select(
+        col('ts').alias('ts'),
+        col('userId').alias('user_id'),
+        col('level').alias('level'),
+        col('song_id').alias('song_id'),
+        col('artist_id').alias('artist_id'),
+        col('ssessionId').alias('session_id'),
+        col('location').alias('location'),
+        col('userAgent').alias('user_agent'),
+        col('year').alias('year'),
+        month('datetime').alias('month')
+    )
+
+
+    songplays_table = songplays_table.selectExpr("ts as start_time")
+    songplays_table.select(monotonically_increasing_id().alias('songplay_id')).collect()
+    
+    # write songplays table to parquet files partitioned by year and month
+    songplays_table.write.partitionBy('year', 'month').parquet(os.path.join(output_data, 'songplays.parquet'), 'overwrite')
+    print("songplays.parquet completed")
+    print("process_log_data completed")
+    
+def main():
+    spark = create_spark_session()
+    input_data = "s3a://udacity-dend/"
+    output_data = "s3://aws-logs-873710589365-us-east-1/elasticmapreduce/"
+    
+    process_song_data(spark, input_data, output_data)    
+    process_log_data(spark, input_data, output_data)
+
+
+if __name__ == "__main__":
+    main()
